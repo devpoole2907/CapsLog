bf9034400c2540866d9d8d73f3a6abba9ca893dd
James Poole <jamesdpoole@icloud.com>
2026-06-07 16:51:36 +1200

    Initial Commit

---
+++ CapsLog/CapsLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapsLogApp.swift
+//  CapsLog
+//
+//  Created by James Poole on 07/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CapsLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
